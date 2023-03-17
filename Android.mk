af43eec55968bc39c2f52d7849ba439a570c1404
betaxab <betaxab@gmail.com>
2023-03-18 04:58:28 +0800

    sm8250: Adjust for our project
    Signed-off-by: betaxab <betaxab@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,3 +1,5 @@
+ifneq ($(filter 4.19, $(TARGET_KERNEL_VERSION)),)
+
 sdm-libs := sdm/libs
 display-hals := include $(sdm-libs)/utils $(sdm-libs)/core libdebug gpu_tonemapper
 
@@ -11,10 +13,5 @@ display-hals += sde-drm
 display-hals += composer
 display-hals += init
 
-ifeq ($(call is-vendor-board-platform,QCOM),true)
-    include $(call all-named-subdir-makefiles,$(display-hals))
-else
-ifneq ($(filter msm% apq%,$(TARGET_BOARD_PLATFORM)),)
-    include $(call all-named-subdir-makefiles,$(display-hals))
-endif
+include $(call all-named-subdir-makefiles,$(display-hals))
 endif
